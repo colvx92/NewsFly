3972b62710b973768ae926e9f8a398ed98e6a998
Sam Abbassi <samabbassi1@gmail.com>
2017-09-26 11:43:14 -0400

    fixed homepage search-prompt and error message

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,8 @@
 class UserController < ApplicationController
   def index
-    parsed = params[:source].split.join('-')
-    @articles = Api.articles(parsed)
+    unless params[:source].blank?
+      parsed = params[:source].split.join('-')
+      @articles = Api.articles(parsed)
+    end
   end
 end
